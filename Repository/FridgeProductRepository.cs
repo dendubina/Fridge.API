145adfe5168ef1a531c3488d60a915db7ce89ecb
RapidRapidRabbit <awestruck31@mail.ru>
2022-08-28 18:27:15 +0300

    serilog

--- Repository/FridgeProductRepository.cs
+++ Repository/FridgeProductRepository.cs
@@ -3,8 +3,6 @@ using Entities.EF;
 using Entities.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
