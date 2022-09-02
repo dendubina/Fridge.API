bf6b062cb78a8568bc33227e3626ffcb4d5f45c6
RapidRapidRabbit <awestruck31@mail.ru>
2022-09-02 03:07:49 +0300

    exception handler

--- Fridge.API/Startup.cs
+++ Fridge.API/Startup.cs
@@ -1,5 +1,6 @@
 using Contracts.Interfaces;
 using Entities.Options;
+using Fridge.API.Extensions;
 using ImageService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -53,6 +54,8 @@ namespace Fridge.API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fridge.API v1"));
             }
+
+            app.ConfigureExceptionHandler();
 
             app.UseHttpsRedirection();
 
