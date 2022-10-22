4de5e27e697cd7bc600e7dca416720fe400bd6d5
RapidRapidRabbit <awestruck31@mail.ru>
2022-10-22 12:14:07 +0300

    tests for auth service

---
+++ AuthMicroService.Tests/Integration tests/Fixtures/AuthWebAppFixture.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using FridgeManager.AuthMicroService;
+using FridgeManager.AuthMicroService.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace AuthMicroService.Tests.Integration_tests.Fixtures
+{
+    public class AuthWebAppFixture : IDisposable
+    {
+        private readonly WebApplicationFactory<Startup> _authAppFactory;
+
+        public HttpClient AuthAppClient { get; }
+
+        public Mock<IAuthService> AuthServiceMock { get; } = new();
+
+        public AuthWebAppFixture()
+        {
+            _authAppFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var authService = services.Single(x => x.ServiceType == typeof(IAuthService));
+                        services.Remove(authService);
+                        services.AddScoped(_ => AuthServiceMock.Object);
+                    });
+                });
+
+            AuthAppClient = _authAppFactory.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            AuthAppClient.Dispose();
+            _authAppFactory.Dispose();
+        }
+    }
+}
