a0c770183e1c14e2edf8b4271f7496d3eac84130
RapidRapidRabbit <awestruck31@mail.ru>
2022-11-22 06:35:08 +0300

    configured azure key vault, azure service bus using when deployed

--- FridgeManager.ProductsMicroService/Extensions/ServiceCollectionsExtensions.cs
+++ FridgeManager.ProductsMicroService/Extensions/ServiceCollectionsExtensions.cs
@@ -3,9 +3,11 @@ using FridgeManager.ProductsMicroService.EF;
 using FridgeManager.ProductsMicroService.Models.Options;
 using FridgeManager.ProductsMicroService.Services;
 using MassTransit;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace FridgeManager.ProductsMicroService.Extensions
 {
@@ -27,19 +29,30 @@ namespace FridgeManager.ProductsMicroService.Extensions
             services.AddScoped<IImageService, ImageService>();
         }
 
-        public static void ConfigureMessageBroker(this IServiceCollection services)
+        public static void ConfigureMessageBroker(this IServiceCollection services, IConfiguration config, IWebHostEnvironment env)
         {
             services.AddMassTransit(x =>
             {
-                x.UsingRabbitMq((context, cfg) =>
+                if (env.IsDevelopment())
                 {
-                    cfg.Host("localhost", "/", h =>
+                    x.UsingRabbitMq((context, cfg) =>
                     {
-                        h.Username("user");
-                        h.Password("user");
+                        cfg.Host("localhost", "/", h =>
+                        {
+                            h.Username("user");
+                            h.Password("user");
+                        });
+                        cfg.ConfigureEndpoints(context);
                     });
-                    cfg.ConfigureEndpoints(context);
-                });
+                }
+                else
+                {
+                    x.UsingAzureServiceBus((context, cfg) =>
+                    {
+                        cfg.Host(config["BusEndpoint"]);
+                        cfg.ConfigureEndpoints(context);
+                    });
+                }
             });
 
             /* services.AddOptions<MassTransitHostOptions>().Configure(options =>
